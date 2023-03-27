bc246eddafa388c0f346c6f5da595bf1767e9bb9
lasha <jimshitashvili4444@gmail.com>
2023-03-27 17:46:52 +0400

    I added the logic of game in project

--- script.js
+++ script.js
@@ -13,12 +13,39 @@ const oTurn = document.querySelector('.o-turn');
 const noCancel = document.querySelector('#quit');
 const yesRestart = document.querySelector('#next-round');
 const homePage = document.querySelector('#homepage');
+const winX = document.querySelector('.pl-2');
+const winO = document.querySelector('.pl-1');
+const roundTied = document.querySelector('.tied');
 
 let player;
 let turn = 'x';
 let freeButtons = [0, 1, 2, 3, 4, 5, 6, 7, 8];
 let xArray = [];
 let oArray = [];
+let winCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+
+const winCheck = () => {
+    for (let i = 0; i < winCombinations.length; i++) {
+        const [a, b, c] = winCombinations[i];
+        if (xArray.includes(a) && xArray.includes(b) && xArray.includes(c)) {
+            winX.style.display = "flex";
+        }else if(oArray.includes(a) && oArray.includes(b) && oArray.includes(c)){
+            winO.style.display = "flex";
+        }else if(freeButtons.length === 0){
+            roundTied.style.display = "flex";
+        }
+    }
+}
+
 
 const drawIcons = () => {
     for (let i = 0; i < box.length; i++) {
@@ -32,18 +59,23 @@ const drawIcons = () => {
             const icon = document.createElement('img');
             icon.classList.add('icon-design');
 
+
             if(turn === 'x'){
                 icon.src = './tic-tac-toe/tic-tac-toe/starter-code/assets/icon-x.svg';
                 event.target.append(icon);
                 turn = 'o';
+                xArray.push(i);
             }else{
                 icon.src = './tic-tac-toe/tic-tac-toe/starter-code/assets/icon-o.svg';
                 event.target.append(icon);
+                oArray.push(i);
                 turn = 'x';
             }
 
             makeHovers();
             event.target.onclick = null;
+            winCheck();
+
         }
             
     }
